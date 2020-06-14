39a825161237aa73ab68eeacdd96f41c283c0171
Ross Baker <rossbaker32@gmail.com>
2020-06-14 16:25:23 +1000

    Simulate JSX with React.createElement()

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // JSX
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+    
+    // JSX gets compiled to React createElement calls
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
