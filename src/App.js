bab1cd60e3e71cf26a39cc38e65054c644462bc5
Ross Baker <rossbaker32@gmail.com>
2020-06-20 21:41:43 +1000

    Add children prop to Person output

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Ross" age="26">My Hobbies: Coding</Person>
+        <Person name="Shanti" age="26"/>
+        <Person name="Bob" age="30"/>
       </div>
     );
     
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 
-const person = () => {
+const person = props => {
   return (
-    <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!!</p>
+    <div>
+      <p>I'm { props.name } and I am { props.age } years old!!</p>
+      <p>{ props.children }</p>
+    </div>
   )
 }
 
