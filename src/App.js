0dc9b3ed5e03fa0a9ec42da254ae0739da80c651
Ross Baker <rossbaker32@gmail.com>
2020-06-21 23:42:47 +1000

    Add a click handler for updating displayed name

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Ross', age: 26 },
+      { name: 'Shanti', age: 26 },
+      { name: 'Bob', age: 30 },
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked');
+    this.setState({persons: [
+      { name: 'Maximillian', age: 26 },
+      { name: 'Shanti', age: 26 },
+      { name: 'Bob', age: 30 },
+    ]})
+  }
+
   render() {
     // JSX
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Ross" age="26">My Hobbies: Coding</Person>
-        <Person name="Shanti" age="26"/>
-        <Person name="Bob" age="30"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobby: Coding</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>  
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>  
       </div>
     );
     
