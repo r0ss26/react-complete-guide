4e9011fb513cb5c987fa39ddf9ff76f365e27f27
Ross Baker <rossbaker32@gmail.com>
2020-06-22 22:03:01 +1000

    Convert class based app component to a functional component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Ross', age: 26 },
       { name: 'Shanti', age: 26 },
       { name: 'Bob', age: 30 },
-    ]
-  }
+    ],
+    otherState: 'some other value'
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log('was clicked');
-    this.setState({persons: [
+    setPersonsState({persons: [
       { name: 'Maximillian', age: 26 },
       { name: 'Shanti', age: 26 },
-      { name: 'Bob', age: 30 },
+      { name: 'Bob', age: 20 },
     ]})
-  }
+  };
 
-  render() {
+  return (
     // JSX
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobby: Coding</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>  
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>  
-      </div>
-    );
-    
-    // JSX gets compiled to React createElement calls
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-  }
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Hobby: Coding</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}></Person>  
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}></Person>  
+    </div>
+  )// JSX gets compiled to React createElement calls
+  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 }
 
 export default App;
